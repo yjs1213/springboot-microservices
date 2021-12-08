d9ad97f0384e77af9dfff325dbdc93e61dfb1a87
유지상 <jidbyoo@gmail.com>
2021-12-08 23:28:09 +0900

    Create MultiplicationService.java

---
+++ src/main/java/microservices/book/socialmultiplication/multiplication/service/MultiplicationService.java
@@ -0,0 +1,15 @@
+package microservices.book.socialmultiplication.multiplication.service;
+
+import microservices.book.socialmultiplication.multiplication.domain.Multiplication;
+
+public interface MultiplicationService {
+
+  /**
+   * 두 개의 무작위 인수를 인수를 담은 {@link Multiplication} 객체를 생성한다.
+   * 무작위로 생성되는 숫자의 범위는 11~99.
+   *
+   * @return 무작위 인수를 담은 {@link Multiplication} 객체
+   */
+  Multiplication createRandomMultiplication();
+
+}
