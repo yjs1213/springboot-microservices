5af579476ba9c077ab2572c8f996f60e28cde35a
유지상 <jidbyoo@gmail.com>
2021-12-08 23:28:15 +0900

    Create RandomGeneratorService.java

---
+++ src/main/java/microservices/book/socialmultiplication/multiplication/service/RandomGeneratorService.java
@@ -0,0 +1,10 @@
+package microservices.book.socialmultiplication.multiplication.service;
+
+public interface RandomGeneratorService {
+
+  /**
+   * @return 무작위로 만든 11 이상 99 이하의 인수
+   */
+  int generateRandomFactor();
+
+}
